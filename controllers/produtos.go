84a2421643f212bd64e2ea260be7d53907ca8948
WilkerAlves <wilkeralves97@gmail.com>
2021-08-22 12:59:46 -0300

    finalizando curso

--- controllers/produtos.go
+++ controllers/produtos.go
@@ -1,7 +1,9 @@
 package controllers
 
 import (
+	"log"
 	"net/http"
+	"strconv"
 	"text/template"
 
 	"github.com/wilker/loja/models"
@@ -12,4 +14,76 @@ var temp = template.Must(template.ParseGlob("templates/*.html"))
 func Index(w http.ResponseWriter, r *http.Request) {
 	todosOsProdutos := models.BuscaTodosOsProdutos()
 	temp.ExecuteTemplate(w, "Index", todosOsProdutos)
+}
+
+func New(w http.ResponseWriter, r *http.Request) {
+	temp.ExecuteTemplate(w, "New", nil)
+}
+
+func Insert(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "POST" {
+		nome := r.FormValue("nome")
+		descricao := r.FormValue("descricao")
+		preco := r.FormValue("preco")
+		quantidade := r.FormValue("quantidade")
+
+		precoParaFloat, err := strconv.ParseFloat(preco, 64)
+		if err != nil {
+			log.Println("Erro na conversao do preco:", err)
+		}
+
+		quantidadeParaInt, err := strconv.Atoi(quantidade)
+		if err != nil {
+			log.Println("Erro na conversao da quantidade:", err)
+		}
+
+		models.CriarNovoProduto(nome, descricao, precoParaFloat, quantidadeParaInt)
+	}
+
+	http.Redirect(w, r, "/", 301)
+}
+
+func Delete(w http.ResponseWriter, r *http.Request) {
+	idDoProduto := r.URL.Query().Get("id")
+	idDoProdutoParaInt, err := strconv.Atoi(idDoProduto)
+	if err != nil {
+		log.Println("Erro na conversao id:", err)
+	}
+	models.DeletaProduto(idDoProdutoParaInt)
+	http.Redirect(w, r, "/", 301)
+}
+
+func Edit(w http.ResponseWriter, r *http.Request) {
+	idDoProduto := r.URL.Query().Get("id")
+	produto := models.EditaProduto(idDoProduto)
+	temp.ExecuteTemplate(w, "Edit", produto)
+}
+
+func Update(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "POST" {
+		id := r.FormValue("id")
+		nome := r.FormValue("nome")
+		descricao := r.FormValue("descricao")
+		preco := r.FormValue("preco")
+		quantidade := r.FormValue("quantidade")
+
+		precoParaFloat, err := strconv.ParseFloat(preco, 64)
+		if err != nil {
+			log.Println("Erro na conversao do preco:", err)
+		}
+
+		quantidadeParaInt, err := strconv.Atoi(quantidade)
+		if err != nil {
+			log.Println("Erro na conversao da quantidade:", err)
+		}
+
+		idParaInt, err := strconv.Atoi(id)
+		if err != nil {
+			log.Println("Erro na conversao do id:", err)
+		}
+
+		models.AtualizaProduto(idParaInt, nome, descricao, precoParaFloat, quantidadeParaInt)
+	}
+
+	http.Redirect(w, r, "/", 301)
 }
--- models/produtos.go
+++ models/produtos.go
@@ -40,3 +40,62 @@ func BuscaTodosOsProdutos() []Produto {
 	defer db.Close()
 	return produtos
 }
+
+func CriarNovoProduto(nome, descricao string, preco float64, quantidade int) {
+	db := db.ConectaComBancoDeDados()
+	insereDadoNoBanco, err := db.Prepare("insert into produtos (nome, descricao, preco, quantidade) values(?, ?, ?, ?)")
+	if err != nil {
+		panic(err.Error())
+	}
+	insereDadoNoBanco.Exec(nome, descricao, preco, quantidade)
+	defer db.Close()
+}
+
+func DeletaProduto(id int) {
+	db := db.ConectaComBancoDeDados()
+	deletaDadoNoBanco, err := db.Prepare("delete from produtos where id = ?")
+	if err != nil {
+		panic(err.Error())
+	}
+	deletaDadoNoBanco.Exec(id)
+	defer db.Close()
+}
+
+func EditaProduto(id string) Produto {
+	db := db.ConectaComBancoDeDados()
+	produtoDoBanco, err := db.Query("select * from produtos where id = ?", id)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	produtoParaAtualizar := Produto{}
+	for produtoDoBanco.Next() {
+		var id, quantidade int
+		var nome, descricao string
+		var preco float64
+
+		err = produtoDoBanco.Scan(&id, &nome, &descricao, &preco, &quantidade)
+		if err != nil {
+			panic(err.Error())
+		}
+
+		produtoParaAtualizar.Id = id
+		produtoParaAtualizar.Nome = nome
+		produtoParaAtualizar.Descricao = descricao
+		produtoParaAtualizar.Preco = preco
+		produtoParaAtualizar.Quantidade = quantidade
+	}
+
+	defer db.Close()
+	return produtoParaAtualizar
+}
+
+func AtualizaProduto(id int, nome, descricao string, preco float64, quantidade int) {
+	db := db.ConectaComBancoDeDados()
+	insereDadoNoBanco, err := db.Prepare("update produtos set nome = ?, descricao = ?, preco = ?, quantidade = ? where id = ? ")
+	if err != nil {
+		panic(err.Error())
+	}
+	insereDadoNoBanco.Exec(nome, descricao, preco, quantidade, id)
+	defer db.Close()
+}
